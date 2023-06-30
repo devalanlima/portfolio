c05d06b36bb855b0192b31b62ff04b33dff8febd
Alan Lima <dev.alanlima@gmail.com>
2023-06-30 11:04:14 -0300

    add / base colors and base fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue, js, ts, jsx, tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        preto:'#0d0d0d',
+        branco:'#F8F8F8',
+      },
+      fontFamily:{
+        stretch: ['Stretch', 'sans-serif'],
+        roboto: ['Roboto Mono', 'monospace'],
+      }
+    },
   },
   plugins: [],
 }
