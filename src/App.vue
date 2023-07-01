a612941cbae1c70dd5e8c5e5d4d45345449a6e40
Alan Lima <dev.alanlima@gmail.com>
2023-06-30 21:33:05 -0300

    add / main and SectionOne

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { onMounted } from 'vue';
 import NavBar from './components/NavBar.vue';
+import SectionOne from './components/SectionOne.vue';
 
-onMounted(() => {
+/* onMounted(() => {
   // On page load or when changing themes, best to add inline in `head` to avoid FOUC
   if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark')
@@ -15,11 +16,14 @@ onMounted(() => {
   localStorage.theme = 'dark'
   // Whenever the user explicitly chooses to respect the OS preference
   localStorage.removeItem('theme')
-})
+}) */
 </script>
 
 <template>
   <header>
     <NavBar />
   </header>
+  <main class="px-5">
+    <SectionOne class="mt-6"/>
+  </main>
 </template>
