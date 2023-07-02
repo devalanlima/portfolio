408f7d924c4f3d9a4ffb69819c1f6b3a91eb94ac
Alan Lima <dev.alanlima@gmail.com>
2023-07-02 10:51:16 -0300

    create / KnowledgeTags.vue component

---
+++ src/components/KnowledgeTags.vue
@@ -0,0 +1,16 @@
+<template>
+    <div :class="['py-1 px-3 w-fit rounded-lg font-mono hover:scale-105 hover:transition-all hover:duration-100 hover:font-semibold cursor-default', 
+    {'bg-[#E8D44D] text-preto': props.value === 'JavaScript'},
+    {'bg-[#0661AE] text-branco': props.value === 'CSS'},
+    {'bg-[#42B883] text-[#35495E]': props.value === 'VueJS'},
+    {'bg-[#DD4B24] text-branco': props.value === 'HTML'},
+    {'bg-branco text-[#0661AE] border-[1px] border-[#0661AE]': props.value === 'TailwindCSS'},
+    ]">
+        {{ props.value }}
+    </div>
+</template>
+<script setup>
+const props = defineProps({
+    value:{type:String, default:'JavaScript'}
+})
+</script>
