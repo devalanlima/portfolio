15557bdc293a4d67cd553552211bfc89b73b0677
Alan Lima <dev.alanlima@gmail.com>
2023-07-02 10:54:07 -0300

    add / knowledge tags for each project

--- src/components/CarouselProjects.vue
+++ src/components/CarouselProjects.vue
@@ -15,46 +15,59 @@
             </div>
         </div>
         <div class="hidden items-center justify-center gap-6 mt-3">
-            <button @click="prev"><svg class="rotate-180 transition-colors ease-in-out duration-[2000ms] -ml-[5px] fill-branco dark:fill-preto stroke-preto dark:stroke-branco" width="28" height="29"
-                    viewBox="0 -1 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <button @click="prev"><svg
+                    class="rotate-180 transition-colors ease-in-out duration-[2000ms] -ml-[5px] fill-branco dark:fill-preto stroke-preto dark:stroke-branco"
+                    width="28" height="29" viewBox="0 -1 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M15.0208 1H2L13.9792 13.5L2 26H15.0208L27 13.5L15.0208 1Z" />
                 </svg>
             </button>
-            <button @click="next"><svg class="transition-colors ease-in-out duration-[2000ms] -ml-[5px] fill-branco dark:fill-preto stroke-preto dark:stroke-branco" width="28" height="30"
-                    viewBox="0 -1 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <button @click="next"><svg
+                    class="transition-colors ease-in-out duration-[2000ms] -ml-[5px] fill-branco dark:fill-preto stroke-preto dark:stroke-branco"
+                    width="28" height="30" viewBox="0 -1 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M15.0208 1H2L13.9792 13.5L2 26H15.0208L27 13.5L15.0208 1Z" />
                 </svg>
             </button>
+        </div>
+        <div class="flex gap-2 mt-2 w-full flex-wrap justify-center">
+            <template v-for="tag in atualTags" :key="tag">
+                <KnowledgeTags :value="tag" />
+            </template>
         </div>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import KnowledgeTags from './KnowledgeTags.vue';
+import { computed, onMounted, ref } from 'vue'
 const carouselContainer = ref(null)
 const carouselSize = 3
-const prev = () => {
-    const carouselItemWidth = carouselContainer.value.clientWidth / carouselSize
-    if (x.value === 0) {
-        x.value = carouselContainer.value.clientWidth - carouselItemWidth
-    } else {
-        x.value -= carouselItemWidth
-    }
 
+const carouselItemWidth = ref('')
+onMounted(() => {
+    carouselItemWidth.value = carouselContainer.value.clientWidth / carouselSize
+})
+
+const prev = () => {
+    carouselItemWidth.value = carouselContainer.value.clientWidth / carouselSize
+    if (x.value === 0) {
+        x.value = carouselContainer.value.clientWidth - carouselItemWidth.value
+    } else {
+        x.value -= carouselItemWidth.value
+    }
 }
 const next = () => {
-    const carouselItemWidth = carouselContainer.value.clientWidth / carouselSize
-    if (x.value === carouselContainer.value.clientWidth - carouselItemWidth) {
+    carouselItemWidth.value = carouselContainer.value.clientWidth / carouselSize
+    if (x.value === carouselContainer.value.clientWidth - carouselItemWidth.value) {
         x.value = 0
     } else {
-        x.value += carouselItemWidth
+        x.value += carouselItemWidth.value
     }
 }
 
 import { useScroll } from '@vueuse/core'
 
 const el = ref(null)
-const { x, y } = useScroll(el)
+const { x } = useScroll(el)
 
 const teste = (e) => {
     e.preventDefault()
@@ -66,6 +79,31 @@ const teste = (e) => {
     }
 
 }
+
+const atualProject = computed(() => {
+    if (x.value >= 0 && x.value < carouselItemWidth.value) {
+        return 'Nice'
+    } else if (x.value >= carouselItemWidth.value && x.value < carouselItemWidth.value * 2) {
+        return 'Equator'
+    } else if (x.value >= carouselItemWidth.value * 2) {
+        return 'PokexChange'
+    } else {
+        return 'error'
+    }
+})
+
+const atualTags = computed(() => {
+    switch (atualProject.value) {
+        case 'Nice':
+            return ['VueJS', 'JavaScript', 'TailwindCSS', 'HTML', 'CSS']
+        case 'Equator':
+            return ['VueJS', 'JavaScript', 'TailwindCSS', 'HTML', 'CSS']
+        case 'PokexChange':
+            return ['VueJS', 'JavaScript', 'HTML', 'CSS']
+        default:
+            return 'error'
+    }
+})
 </script>
 
 <style scoped>
