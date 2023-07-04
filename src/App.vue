c795ae86e5c264d9df5d31abbb174a5804bc51be
Alan Lima <dev.alanlima@gmail.com>
2023-07-04 12:55:04 -0300

    add / sections

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
 import { onMounted } from 'vue';
 import NavBar from './components/NavBar.vue';
-import SectionOne from './components/SectionOne.vue';
+import SectionOne from './components/sections/SectionOne.vue';
+import SectionTwo from './components/sections/SectionTwo.vue';
+import SectionThree from './components/sections/SectionThree.vue';
+import MainFooter from './components/MainFooter.vue';
+import MainModal from './components/common/MainModal.vue';
 
 /* onMounted(() => {
   // On page load or when changing themes, best to add inline in `head` to avoid FOUC
@@ -17,13 +21,21 @@ import SectionOne from './components/SectionOne.vue';
   // Whenever the user explicitly chooses to respect the OS preference
   localStorage.removeItem('theme')
 }) */
+import { ref } from 'vue'
 </script>
 
 <template>
-  <header>
-    <NavBar />
+  <header class="fixed top-0 w-full z-50">
+    <NavBar/>
   </header>
-  <main class="px-5">
-    <SectionOne class="mt-6"/>
+  <main class="px-5 ">
+    <SectionOne class="my-7 mt-14"/>
+    <hr class="border-t-2 border-preto dark:border-branco">
+    <SectionTwo class="my-7"/>
+    <hr class="border-t-2 border-preto dark:border-branco">
+    <SectionThree class="my-7"/>
   </main>
+  <footer class="z-10 relative">
+    <MainFooter class="-mt-[1.9rem]"/>
+  </footer>
 </template>
