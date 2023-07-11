eff00eb582d134fa8f44a59af492524cc0cf0ecc
Alan Lima <dev.alanlima@gmail.com>
2023-07-11 13:39:25 -0300

    stylize / add breakpoints for responsive design

--- src/components/sections/SectionTwo.vue
+++ src/components/sections/SectionTwo.vue
@@ -1,15 +1,33 @@
 <template>
-    <article class="relative overflow-hidden">
-        <div class="text-center">
+    <article class="relative overflow-hidden max-w-[1311px] mx-auto" ref="el">
+        <div class="text-center md:text-left px-5">
             <h3 class="sr-only">Projetos</h3>
-            <span class="font-stretch text-[2.1rem]">PPROJETOS</span>
+            <span class="font-stretch text-[2.1rem]" :style="titleSize">PPROJETOS</span>
         </div>
-        <CarouselProjects class="mt-5" />
-        <GlobeImage class="hidden fill-branco dark:fill-preto bg-preto dark:bg-branco rounded-full absolute top-14 -right-10 -z-10 rotate-[205deg] w-[300px] "/>
+        <CarouselProjects class="mt-14" />
+        <GlobeImage
+            class="hidden md:block fill-branco dark:fill-preto bg-preto dark:bg-branco rounded-full absolute top-0 right-[10%] -z-10 rotate-[205deg] w-[300px] " />
     </article>
 </template>
 
 <script setup>
 import CarouselProjects from '../CarouselProjects.vue';
 import GlobeImage from '../ui/GlobeImage.vue';
+import { ref, computed } from 'vue'
+
+import { useElementSize } from '@vueuse/core'
+const el = ref(null)
+const { width } = useElementSize(el)
+
+const titleSize = computed(() => {
+    if (windowWidth.value < 768) {
+        return {fontSize: `${(33*width.value)/375}px`}
+    } else {
+        return { fontSize: `${(18 * width.value) / 375}px` }
+    }
+}) 
+
+import { useWindowSize } from '@vueuse/core'
+
+const { width: windowWidth } = useWindowSize()
 </script>
