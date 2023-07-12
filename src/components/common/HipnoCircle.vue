46d878a3a8433716b41d18a9477fe06bb04d2445
Alan Lima <dev.alanlima@gmail.com>
2023-07-12 16:23:27 -0300

    create / HipnoCircle.vue component

---
+++ src/components/common/HipnoCircle.vue
@@ -0,0 +1,42 @@
+<template>
+    <div
+        class="w-[44vw] h-[90%] max-w-[600px] max-h-[600px] aspect-auto flex justify-center items-center overflow-hidden">
+        <div class="relative aspect-square h-full grid place-items-center" ref="el">
+            <div v-for="(circle, index) in circleNumbers" :key="circle"
+                class="absolute circleAnimation h-[110%] aspect-square border-[12px] outline outline-[12px] outline-branco border-preto rounded-full"
+                :class="`circle${index} -z-[1${index}]`">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const delay = ref(0.3)
+const circleNumbers = ref(17)
+const el = ref(null)
+onMounted(()=>{
+    const allCircles = [...el.value.children]
+    allCircles.forEach((element, index) => {
+        if(element.classList.contains(`circle${index}`)){
+            element.style.animationDelay = `${delay.value * index}s`
+        }
+    });
+})
+</script>
+
+<style>
+.circleAnimation {
+    animation: scaleCircle 4.5s infinite linear;
+}
+
+@keyframes scaleCircle {
+    0% {
+        height: 0%;
+    }
+    100% {
+        height: 145%;
+    }
+}
+</style>
