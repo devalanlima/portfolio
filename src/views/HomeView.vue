d2a886c7af437b7c707fecf3c665d4fea19d6def
Alan Lima <dev.alanlima@gmail.com>
2023-07-12 16:24:26 -0300

    stylize

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,9 +5,8 @@ import SectionOne from '../components/sections/SectionOne.vue';
 import SectionTwo from '../components/sections/SectionTwo.vue';
 import SectionThree from '../components/sections/SectionThree.vue';
 import MainFooter from '../components/MainFooter.vue';
-import MainModal from '../components/common/MainModal.vue';
 
-/* onMounted(() => {
+onMounted(() => {
   // On page load or when changing themes, best to add inline in `head` to avoid FOUC
   if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark')
@@ -20,20 +19,34 @@ import MainModal from '../components/common/MainModal.vue';
   localStorage.theme = 'dark'
   // Whenever the user explicitly chooses to respect the OS preference
   localStorage.removeItem('theme')
-}) */
+})
+
+
 import { ref } from 'vue'
+const isMobile = ref(false)
+onMounted(() => {
+  function isMobileDevice() {
+    return /Mobile|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  }
+
+  if (isMobileDevice()) {
+    isMobile.value = true
+  } else {
+    isMobile.value = false
+  }
+})
 </script>
 
 <template>
   <header class="fixed top-0 w-full z-50">
-    <NavBar class="w-full"/>
+    <NavBar class="w-full" />
   </header>
-  <main>
-    <SectionOne class="my-14 mt-14 px-5"/>
-    <SectionTwo class="my-24"/>
-    <SectionThree class="-mb-1 mt-24 px-5 overflow-hidden"/>
+  <main :class="['w-screen overflow-hidden', {'pr-3' : !isMobile}]">
+    <SectionOne class="my-14 mt-20 px-5" />
+    <SectionTwo class="my-14" />
+    <SectionThree class="-mb-1 mt-0 px-5" />
   </main>
   <footer class="z-10 relative">
-    <MainFooter/>
+    <MainFooter />
   </footer>
 </template>
