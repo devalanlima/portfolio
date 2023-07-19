68275f524d8f2a86c106fc470bb2fdb8fac9be42
Alan Lima <dev.alanlima@gmail.com>
2023-07-19 10:34:32 -0300

    fix / max number of characters

--- src/components/sections/SectionThree.vue
+++ src/components/sections/SectionThree.vue
@@ -12,7 +12,7 @@
                 class="w-full gap-7 pr-7 xl:pr-0 max-w-[70vw] md:max-w-[43vw] xl:max-w-[550px] grid grid-cols-1 xl:grid-cols-2 xl:justify-between"
                 action="https://formsubmit.co/d508eed6ee546479cd0f3667fecc4fb5" method="POST" :onsubmit="validateForm">
                 <input type="hidden" name="_captcha" value="false"> <!-- Disable reCAPTCHA -->
-                <input type="hidden" name="_next" value="http://localhost:5173/sucesso"> <!-- alternative url -->
+                <input type="hidden" name="_next" value="https://devalanlima.github.io/portfolio/#Contatos"> <!-- alternative url -->
                 <ul class="flex flex-col gap-10 ">
                     <li class="relative">
                         <label for="contact-name"
@@ -33,7 +33,7 @@
                     <li class="relative">
                         <label for="contact-message"
                             :class="['absolute -top-4 text-sm font-roboto font-light text-red-600 dark:text-red-400', { 'hidden': mensagemIsValid }]">Mínimo
-                            de 35 caractéres</label>
+                            de 30 caractéres</label>
                         <textarea name="user_message" id="contact-message" cols="40" rows="3" required
                             :class="['placeholder:dark:text-branco dark:text-branco dark:border-branco  bg-transparent font-roboto  text-preto placeholder:text-preto placeholder:font-light focus:outline-0 -mt-6 bg-[repeating-linear-gradient(var(--color1),var(--color1)_50px,var(--color2)_50px,var(--color2)_52px,var(--color1)_51px)] dark:bg-[repeating-linear-gradient(var(--color2),var(--color2)_50px,var(--color1)_50px,var(--color1)_52px,var(--color2)_51px)] h-[156px] bg-local leading-[51px] pt-[10px] resize-none w-full', { 'text-red-600 dark:text-red-400': !emailIsValid }]"
                             v-model="suaMensagem" placeholder="Sua Mensagem"></textarea>
@@ -77,8 +77,8 @@ const upBtn = () => {
 
 const seuNome = ref('')
 const nomeIsValid = computed(() => {
-    const nomeRegex = /[a-zA-Z]{3,}/
-    if (nomeRegex.test(seuNome.value) || seuNome.value.length === 0) {
+    const nomeRegex = /[a-zA-Z]/
+    if ((nomeRegex.test(seuNome.value) && seuNome.value.length >= 3) || seuNome.value.length === 0) {
         return true
     } else {
         return false
@@ -97,8 +97,8 @@ const emailIsValid = computed(() => {
 
 const suaMensagem = ref('')
 const mensagemIsValid = computed(() => {
-    const nomeRegex = /[a-zA-Z]{35,}/
-    if (nomeRegex.test(suaMensagem.value) || suaMensagem.value.length === 0) {
+    const nomeRegex = /[a-zA-Z]/
+    if ((nomeRegex.test(suaMensagem.value) && suaMensagem.value.length >= 30) || suaMensagem.value.length === 0) {
         return true
     } else {
         return false
