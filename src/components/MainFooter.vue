81dcbe264fdc3959d381c3b1dfd88349b4d2cbbe
Alan Lima <dev.alanlima@gmail.com>
2023-08-21 12:39:56 -0300

    refactor / change text content and add link to portfolio

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,3 +1,3 @@
 <template>
-    <p class="font-roboto text-xs border-t-2 border-preto py-6 text-center dark:border-branco bg-branco dark:bg-preto">Criado e desenvolvido por Alan Pereira</p>
+    <p class="font-roboto text-xs border-t-2 border-preto py-6 text-center dark:border-branco bg-branco dark:bg-preto">Criado e desenvolvido por <a href="https://devalanlima.github.io/portfolio/">Alan Lima</a></p>
 </template>
