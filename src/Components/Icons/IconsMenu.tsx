3c162cac8ed9c7e54d2d4e03eaab3de246e66295
Alan Lima <dev.alanlima@gmail.com>
2024-10-21 13:58:28 -0300

    style: remove unnecessary style prop

--- src/Components/Icons/IconsMenu.tsx
+++ src/Components/Icons/IconsMenu.tsx
@@ -12,10 +12,7 @@ export default function IconsMenu({ className }: Props) {
         xmlns="http://www.w3.org/2000/svg"
         viewBox="-0.5 -0.5 24 24"
       >
-        <path
-          d="M2.875 17.25v-1.4375h5.75v1.4375H2.875Zm0 -5.03125v-1.4375h11.5v1.4375H2.875ZM2.875 7.1875v-1.4375h17.25v1.4375H2.875Z"
-          stroke-width="1"
-        />
+        <path d="M2.875 17.25v-1.4375h5.75v1.4375H2.875Zm0 -5.03125v-1.4375h11.5v1.4375H2.875ZM2.875 7.1875v-1.4375h17.25v1.4375H2.875Z" />
       </svg>
     </>
   );
