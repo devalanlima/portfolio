f1e573d216c47d50c9212790dbba06db3303af3e
Alan Lima <dev.alanlima@gmail.com>
2024-10-21 15:34:28 -0300

    style: change double quotes to single quotes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
     colors: {
-      'primary': 'rgb(var(--primary-color))',
-      'secondary': 'rgb(var(--secondary-color))',
-      'white': 'rgb(var(--white))',
-      'black': 'rgb(var(--black))',
-      'typescript': 'rgb(var(--typescript))',
-      'javascript': 'rgb(var(--javascript))',
+      primary: 'rgb(var(--primary-color))',
+      secondary: 'rgb(var(--secondary-color))',
+      white: 'rgb(var(--white))',
+      black: 'rgb(var(--black))',
+      typescript: 'rgb(var(--typescript))',
+      javascript: 'rgb(var(--javascript))',
       'vue-1': 'rgb(var(--vue-1))',
       'vue-2': 'rgb(var(--vue-2))',
-      'react': 'rgb(var(--react))',
-      'tailwindcss': 'rgb(var(--tailwindcss))',
-      'html': 'rgb(var(--html))',
-      'css': 'rgb(var(--css))',
+      react: 'rgb(var(--react))',
+      tailwindcss: 'rgb(var(--tailwindcss))',
+      html: 'rgb(var(--html))',
+      css: 'rgb(var(--css))',
       'git-1': 'rgb(var(--git-1))',
       'git-2': 'rgb(var(--git-2))',
       'photoshop-1': 'rgb(var(--photoshop-1))',
@@ -25,11 +23,11 @@ export default {
     },
     extend: {
       fontFamily: {
-        AbrilFatface: ["Abril Fatface", "serif"],
-        CourierPrime: ["Courier Prime", "monospace"],
-        DelaGothicOne: ["Dela Gothic One", "sans-serif"],
+        AbrilFatface: ['Abril Fatface', 'serif'],
+        CourierPrime: ['Courier Prime', 'monospace'],
+        DelaGothicOne: ['Dela Gothic One', 'sans-serif'],
       },
     },
   },
   plugins: [],
-}
+};
