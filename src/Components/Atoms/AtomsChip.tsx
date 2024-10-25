0e37a28413ca3550dd8f47e90b8c681e843bdd95
Alan Lima <dev.alanlima@gmail.com>
2024-10-25 16:24:29 -0300

    refactor: now each chip has a theme and style instead of a children

--- src/Components/Atoms/AtomsChip.tsx
+++ src/Components/Atoms/AtomsChip.tsx
@@ -1,15 +1,38 @@
+import { twMerge } from 'tailwind-merge';
+import { Themes } from '../../types/themes';
+
 interface Props {
-  children: React.ReactNode;
+  skill?: Themes;
+  color?: boolean;
 }
 
-export default function AtomsChip({ children }: Props) {
+export default function AtomsChip({ skill = 'dark', color = false }: Props) {
+  const chipStyle: Record<Themes, string> = {
+    light: 'bg-secondary text-primary outline-primary',
+    dark: 'bg-primary text-secondary outline-secondary',
+    typescript: 'bg-typescript text-white outline-white',
+    javascript: 'bg-javascript text-black outline-black',
+    vue: 'bg-vue-1 text-vue-2 outline-vue-2',
+    react: 'bg-white text-react outline-react',
+    tailwindcss: 'bg-tailwindcss text-white outline-white',
+    html: 'bg-html text-white outline-white',
+    css: 'bg-css text-white outline-white',
+    git: 'bg-git-1 text-git-2 outline-git-2',
+    photoshop: 'bg-photoshop-1 text-photoshop-2 outline-photoshop-2',
+    figma: 'bg-black text-white outline-white',
+    github: 'bg-black text-white outline-white',
+  };
+
   return (
     <div
-      className={
-        'px-3 flex py-1 bg-secondary text-primary rounded-full w-fit font-CourierPrime text-xs leading-none outline-primary outline-2 font-bold outline cursor-pointer hover:scale-105'
-      }
+      className={twMerge(
+        'px-3 flex py-1 bg-primary outline-secondary text-secondary hover:bg-secondary hover:text-primary hover:outline-primary rounded-full w-fit font-CourierPrime text-xs leading-none outline-2 font-bold outline cursor-pointer hover:scale-105',
+        color && chipStyle[skill]
+      )}
     >
-      <span className="mt-1">{children}</span>
+      <span className="mt-1">
+        {skill.charAt(0).toUpperCase() + skill.slice(1)}
+      </span>
     </div>
   );
 }
