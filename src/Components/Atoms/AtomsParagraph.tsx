63da0d0f827b28aa054eb8b6e6498de1e5573d03
Alan Lima <dev.alanlima@gmail.com>
2024-10-30 16:57:18 -0300

    refactor: extend props and add twMerge on style

--- src/Components/Atoms/AtomsParagraph.tsx
+++ src/Components/Atoms/AtomsParagraph.tsx
@@ -1,10 +1,23 @@
-interface Props {
+import { twMerge } from 'tailwind-merge';
+
+interface Props extends React.HTMLAttributes<HTMLParagraphElement> {
   children: React.ReactNode;
+  className?: string;
 }
 
-export default function AtomsParagraph({ children }: Props) {
+export default function AtomsParagraph({
+  children,
+  className,
+  ...rest
+}: Props) {
   return (
-    <p className="text-sm font-CourierPrime text-primary leading-4">
+    <p
+      {...rest}
+      className={twMerge(
+        'text-sm font-CourierPrime text-primary leading-4',
+        className
+      )}
+    >
       {children}
     </p>
   );
