901665e5ac9d096392f4c34637df47a049f0b396
Alan Lima <dev.alanlima@gmail.com>
2024-10-30 16:55:23 -0300

    refactor: extend props and edit label style

--- src/Components/Atoms/AtomsInputText.tsx
+++ src/Components/Atoms/AtomsInputText.tsx
@@ -1,11 +1,11 @@
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
-  label?: string;
+  label?: React.ReactNode;
 }
 
 export default function AtomsInputText({ label, ...rest }: Props) {
   return (
-    <label className="flex gap-2 border-b-2 border-primary p-2 bg-none">
-      {label}
+    <label className="flex flex-col gap-2 border-b-2 border-primary p-2 bg-none relative">
+      <p className="text-xs absolute -top-3 left-1">{label}</p>
       <input
         className="placeholder:text-primary focus:placeholder:text-secondary font-CourierPrime border-none outline-none bg-secondary text-primary"
         {...rest}
--- src/Components/Atoms/AtomsInputTextarea.tsx
+++ src/Components/Atoms/AtomsInputTextarea.tsx
@@ -1,11 +1,11 @@
 interface Props extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
-  label?: string;
+  label?: React.ReactNode;
 }
 
 export default function AtomsInputTextarea({ label, ...rest }: Props) {
   return (
-    <label>
-      {label}
+    <label className="relative flex flex-col gap-2">
+      <p className="text-xs absolute -top-0 left-1">{label}</p>
       <textarea
         {...rest}
         className="p-2 font-CourierPrime  text-primary placeholder:text-primary focus:outline-0  bg-[repeating-linear-gradient(rgb(var(--secondary-color)),rgb(var(--secondary-color))_50px,rgb(var(--primary-color))_50px,rgb(var(--primary-color))_52px,rgb(var(--secondary-color))_52px)] bg-local leading-[52px] resize-none w-full overflow-y-auto focus:placeholder:text-secondary"
