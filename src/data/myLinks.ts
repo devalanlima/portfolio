e8d458544c15f089f8511874bfb22fe537143be1
Alan Lima <dev.alanlima@gmail.com>
2024-10-30 17:10:36 -0300

    feat: create a file with my links and their respective icons

---
+++ src/data/myLinks.ts
@@ -0,0 +1,28 @@
+import IconsFilePDF from "../Components/Icons/IconsFilePDF";
+import IconsGithub from "../Components/Icons/IconsGithub";
+import IconsLinkedin from "../Components/Icons/IconsLinkedin";
+import IconsWhatsapp from "../Components/Icons/IconsWhatsapp";
+import { LinkIcon } from "../types/linkIcon";
+
+export const links: Array<LinkIcon> = [
+  {
+    name: 'linkedin',
+    url: 'https://www.linkedin.com/in/devalanlima/',
+    icon: IconsLinkedin,
+  },
+  {
+    name: 'whatsapp',
+    url: 'https://wa.me/55021996058924',
+    icon: IconsWhatsapp,
+  },
+  {
+    name: 'github',
+    url: 'https://github.com/devalanlima',
+    icon: IconsGithub,
+  },
+  {
+    name: 'curriculo',
+    url: '/Alan-Lima-Curriculo.pdf',
+    icon: IconsFilePDF,
+  },
+];
