fb09e67cc08ecad392a6c1cd5b4a8ef0c89162cd
Alan Lima <dev.alanlima@gmail.com>
2024-11-01 16:52:35 -0300

    feat: add className prop with twMerge

--- src/Components/Atoms/AtomsBaseButton.tsx
+++ src/Components/Atoms/AtomsBaseButton.tsx
@@ -1,11 +1,18 @@
 import { useState } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   onClick?: () => void;
   children: React.ReactNode;
+  className?: string;
 }
 
-export default function AtomsBaseButton({ onClick, children, ...rest }: Props) {
+export default function AtomsBaseButton({
+  onClick,
+  children,
+  className,
+  ...rest
+}: Props) {
   const styles = {
     default: 'pb-3 pt-1 pr-3 pl-1 after:-top-1 after:-left-1',
     pressed: 'p-2 after:top-0 after:left-0',
@@ -32,7 +39,10 @@ export default function AtomsBaseButton({ onClick, children, ...rest }: Props) {
       onMouseUp={handleMouseUp}
       onMouseLeave={handleMouseLeave}
       {...rest}
-      className={`border-2 border-primary bg-primary relative  z-0 after:bg-secondary after:absolute after:w-full after:h-full after:-z-10 after:outline after:outline-2 after:outline-primary ${btnStyle}`}
+      className={twMerge(
+        `border-2 border-primary bg-primary relative  z-0 after:bg-secondary after:absolute after:w-full after:h-full after:-z-10 after:outline after:outline-2 after:outline-primary ${btnStyle}`,
+        className
+      )}
     >
       {children}
     </button>
--- src/Components/Atoms/AtomsOutlinedButton.tsx
+++ src/Components/Atoms/AtomsOutlinedButton.tsx
@@ -1,10 +1,20 @@
+import { twMerge } from 'tailwind-merge';
+
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children?: React.ReactNode;
+  className?: string;
 }
 
-export default function AtomsOutlinedButton({ children, ...rest }: Props) {
+export default function AtomsOutlinedButton({
+  children,
+  className,
+  ...rest
+}: Props) {
   return (
-    <button className="outline outline-2 outline-primary p-2" {...rest}>
+    <button
+      className={twMerge('outline outline-2 outline-primary p-2', className)}
+      {...rest}
+    >
       {children}
     </button>
   );
