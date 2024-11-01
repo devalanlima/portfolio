74328bc3c9e484cf5081c876b14468fd488fda8d
Alan Lima <dev.alanlima@gmail.com>
2024-11-01 16:59:55 -0300

    feat: create project page

---
+++ src/Components/Pages/PagesProjetos.tsx
@@ -0,0 +1,23 @@
+import AtomsHeader from '../Atoms/AtomsHeader';
+import TemplatesMain from '../Templates/TemplatesMain';
+import { projects } from '../../data/projects';
+import OrganismsProjectCard from '../Organisms/OrganismsProjectCard';
+
+export default function PagesProjetos() {
+  return (
+    <TemplatesMain>
+      <main className="px-4 py-6 flex flex-col gap-6">
+        <section className="flex flex-col gap-6">
+          <AtomsHeader smallText="MEUS" bigText="PROJETOS" />
+          <ul className="flex flex-col gap-6">
+            {projects.map(project => (
+              <li key={project.name}>
+                <OrganismsProjectCard project={project} />
+              </li>
+            ))}
+          </ul>
+        </section>
+      </main>
+    </TemplatesMain>
+  );
+}
