1572cc514b6dbc4555122153698b7e4ae2464c38
Alan Lima <dev.alanlima@gmail.com>
2024-11-05 15:49:29 -0300

    feat: create file to store projects

---
+++ src/data/projects.ts
@@ -0,0 +1,53 @@
+import { Project } from "../types/project";
+
+export const projects: Array<Project> = [
+  {
+    name: 'PokePlace',
+    url: 'https://pokeplace.netlify.app/',
+    image: '/pokeplace.png',
+    skills: [
+      'vue',
+      'javascript',
+      'tailwindcss',
+      'css',
+      'html',
+      'git',
+      'github',
+    ],
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, exercitationem quisquam consectetur nulla nobis libero quo ratione culpa, iure eveniet ad animi dolor vero deleniti quis eos, minima quidem sapiente. Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, exercitationem quisquam consectetur nulla nobis libero quo ratione culpa, iure eveniet ad animi dolor vero deleniti quis eos, minima quidem sapiente. Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, exercitationem quisquam consectetur nulla nobis libero quo ratione culpa, iure eveniet ad animi dolor vero deleniti quis eos, minima quidem sapiente.',
+  },
+  {
+    name: 'PokePlace2',
+    url: 'https://pokeplace.netlify.app/',
+    image: '/pokeplace.png',
+    skills: [
+      'vue',
+      'javascript',
+      'tailwindcss',
+      'css',
+      'html',
+      'git',
+      'github',
+    ],
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, exercitationem quisquam consectetur nulla nobis libero quo ratione culpa, iure eveniet ad animi dolor vero deleniti quis eos, minima quidem sapiente. Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, exercitationem quisquam consectetur nulla nobis libero quo ratione culpa, iure eveniet ad animi dolor vero deleniti quis eos, minima quidem sapiente. Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, exercitationem quisquam consectetur nulla nobis libero quo ratione culpa, iure eveniet ad animi dolor vero deleniti quis eos, minima quidem sapiente.',
+  },
+  {
+    name: 'PokePlace3',
+    url: 'https://pokeplace.netlify.app/',
+    image: '/pokeplace.png',
+    skills: [
+      'vue',
+      'javascript',
+      'tailwindcss',
+      'css',
+      'html',
+      'git',
+      'github',
+    ],
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, exercitationem quisquam consectetur nulla nobis libero quo ratione culpa, iure eveniet ad animi dolor vero deleniti quis eos, minima quidem sapiente. Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, exercitationem quisquam consectetur nulla nobis libero quo ratione culpa, iure eveniet ad animi dolor vero deleniti quis eos, minima quidem sapiente. Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, exercitationem quisquam consectetur nulla nobis libero quo ratione culpa, iure eveniet ad animi dolor vero deleniti quis eos, minima quidem sapiente.',
+  },
+];
+
