bb3c5b075e0bd2d205d04b443bccc2cad123c103
Alan Lima <dev.alanlima@gmail.com>
2024-11-05 15:46:47 -0300

    refactor: change render method and add responsiveness

--- src/Components/Molecules/MoleculesNavigationMenu.tsx
+++ src/Components/Molecules/MoleculesNavigationMenu.tsx
@@ -3,14 +3,16 @@ import AtomsOutlinedButton from '../Atoms/AtomsOutlinedButton';
 import IconsMenu from '../Icons/IconsMenu';
 import IconsClose from '../Icons/IconsClose';
 import { Link, RouteObject } from 'react-router-dom';
+import { twMerge } from 'tailwind-merge';
 
 interface Props {
   routes: Array<RouteObject>;
+  className?: string;
 }
 
-export default function MoleculesNavigationMenu({ routes }: Props) {
+export default function MoleculesNavigationMenu({ routes, className }: Props) {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-  const navRef = useRef<HTMLElement | null>(null);
+  const $menu = useRef<HTMLDivElement | null>(null);
   const MenuIcon = isMenuOpen ? IconsClose : IconsMenu;
 
   const toggleMenuView = () => {
@@ -20,8 +22,8 @@ export default function MoleculesNavigationMenu({ routes }: Props) {
   const handleClickOutside = (event: MouseEvent | null) => {
     if (
       event &&
-      navRef.current &&
-      !navRef.current.contains(event.target as Node)
+      $menu.current &&
+      !$menu.current.contains(event.target as Node)
     ) {
       setIsMenuOpen(false);
     }
@@ -36,29 +38,33 @@ export default function MoleculesNavigationMenu({ routes }: Props) {
   }, []);
 
   return (
-    <div className="relative z-50">
-      <AtomsOutlinedButton onClick={toggleMenuView} aria-expanded={isMenuOpen}>
-        <MenuIcon className="w-[28px] h-[28px]" />
+    <div className={twMerge('relative z-50', className)} ref={$menu}>
+      <AtomsOutlinedButton
+        onClick={toggleMenuView}
+        aria-expanded={isMenuOpen}
+        className="sm:hidden"
+      >
+        <MenuIcon className="h-[28px] w-[28px]" />
       </AtomsOutlinedButton>
-      {isMenuOpen && (
-        <nav ref={navRef} className="absolute top-[calc(100%_+2px)] left-0">
-          <ul className="flex flex-col gap-[2px]">
-            {routes.map(
-              route =>
-                route.path && (
-                  <li
-                    key={route.path}
-                    className="outline outline-2 outline-primary bg-secondary hover:bg-primary text-primary hover:text-secondary font-CourierPrime text-lg"
-                  >
-                    <Link className="px-4 py-1 flex" to={route.path}>
-                      {route.id}
-                    </Link>
-                  </li>
-                )
-            )}
-          </ul>
-        </nav>
-      )}
+      <nav
+        className={`absolute left-0 top-[calc(100%_+2px)] sm:relative ${isMenuOpen ? 'block' : 'hidden sm:block'}`}
+      >
+        <ul className="flex flex-col gap-[2px] sm:flex-row">
+          {routes.map(
+            route =>
+              route.path && (
+                <li
+                  key={route.path}
+                  className="bg-secondary pt-1 font-CourierPrime text-lg text-primary outline outline-2 outline-primary hover:bg-primary hover:text-secondary sm:font-semibold sm:outline-none sm:hover:bg-secondary sm:hover:text-primary sm:hover:underline sm:hover:underline-offset-1 md:text-xl"
+                >
+                  <Link className="flex px-4 py-1" to={route.path}>
+                    {route.id}
+                  </Link>
+                </li>
+              )
+          )}
+        </ul>
+      </nav>
     </div>
   );
 }
