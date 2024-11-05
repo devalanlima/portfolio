9c8cfb2bd36cfcca2db4aa20b47f349fa2bba118
Alan Lima <dev.alanlima@gmail.com>
2024-11-05 15:48:52 -0300

    feat: implement responsive design

--- src/Components/Organisms/OrganismsHeader.tsx
+++ src/Components/Organisms/OrganismsHeader.tsx
@@ -5,10 +5,14 @@ import { routes } from '../../Routes/routes';
 
 export default function OrganismsHeader() {
   return (
-    <header className="outline outline-2 outline-primary flex justify-between items-center">
-      <MoleculesNavigationMenu routes={routes} />
-      <AtomsTitleName>ALAN LIMA</AtomsTitleName>
-      <MoleculesDarkModeButton />
+    <header className="outline outline-2 outline-primary">
+      <div className="mx-auto flex max-w-[1440px] items-center justify-between sm:px-5 md:py-2">
+        <MoleculesNavigationMenu className="sm:order-1" routes={routes} />
+
+        <AtomsTitleName className="sm:order-0">ALAN LIMA</AtomsTitleName>
+
+        <MoleculesDarkModeButton className="sm:order-2 sm:outline-none" />
+      </div>
     </header>
   );
 }
