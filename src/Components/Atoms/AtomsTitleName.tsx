f684454e28702f833674a439ee1980328d13840b
Alan Lima <dev.alanlima@gmail.com>
2024-11-05 15:21:20 -0300

    feat: implement responsive design and className in props

--- src/Components/Atoms/AtomsTitleName.tsx
+++ src/Components/Atoms/AtomsTitleName.tsx
@@ -1,13 +1,24 @@
 import { Link } from 'react-router-dom';
+import { twMerge } from 'tailwind-merge';
 
 interface Props {
   children: React.ReactNode;
   path?: string;
+  className?: string;
 }
 
-export default function AtomsTitleName({ children, path = '/' }: Props) {
+export default function AtomsTitleName({
+  children,
+  path = '/',
+  className,
+}: Props) {
   return (
-    <h1 className={'font-AbrilFatface text-xl m-0 text-primary'}>
+    <h1
+      className={twMerge(
+        'm-0 font-AbrilFatface text-xl text-primary md:text-2xl',
+        className
+      )}
+    >
       <Link to={path}>{children}</Link>
     </h1>
   );
