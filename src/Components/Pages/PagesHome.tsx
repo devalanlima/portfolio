684b2992e56e2dbc3bcceae5889d3d1c0be2c98b
Alan Lima <dev.alanlima@gmail.com>
2024-11-06 16:24:02 -0300

    feat: implement responsive design

--- src/Components/Pages/PagesHome.tsx
+++ src/Components/Pages/PagesHome.tsx
@@ -1,11 +1,14 @@
 import { Themes } from '../../types/themes';
-import AtomsHeader from '../Atoms/AtomsHeader';
+import AtomsTitle from '../Atoms/AtomsTitle';
 import AtomsParagraph from '../Atoms/AtomsParagraph';
 import AtomsPeopleSVG from '../Atoms/AtomsPeopleSVG';
 import MoleculesSkills from '../Molecules/MoleculesSkills';
 import TemplatesMain from '../Templates/TemplatesMain';
+import useBreakpoints from '../../hooks/useBreakpoints';
 
 export default function PagesHome() {
+  const breakpoint = useBreakpoints();
+
   const skills: Array<Themes> = [
     'typescript',
     'javascript',
@@ -22,22 +25,27 @@ export default function PagesHome() {
 
   return (
     <TemplatesMain>
-      <main className="flex flex-col gap-6 px-4 py-6">
-        <section className="flex flex-col gap-6">
-          <AtomsHeader smallText="DESENVOLVEDOR" bigText="FRONT-END" />
-          <AtomsParagraph className="text-justify">
-            Meu nome é Alan Lima, sou formado em Automação Industrial e Técnico
-            em Eletrônica, com uma paixão crescente por desenvolvimento web.
-            Atualmente, estou cursando Análise e Desenvolvimento de Sistemas e
-            focando em me especializar como Desenvolvedor Front-End. Estou em
-            busca de oportunidades para aplicar meus conhecimentos e contribuir
-            para projetos inovadores.
-          </AtomsParagraph>
-          <AtomsPeopleSVG />
-        </section>
-        <section>
-          <MoleculesSkills skills={skills} />
-        </section>
+      <main className="grid place-items-center gap-6 px-4 py-6 md:grid-cols-2">
+        <div className="flex max-w-[500px] flex-col gap-6 md:gap-10 lg:max-w-[650px]">
+          <section className="flex flex-col gap-6 md:gap-10">
+            <AtomsTitle smallText="DESENVOLVEDOR" bigText="FRONT-END" />
+            <AtomsParagraph className="text-justify">
+              Meu nome é Alan Lima, sou formado em Automação Industrial e
+              Técnico em Eletrônica, com uma paixão crescente por
+              desenvolvimento web. Atualmente, estou cursando Análise e
+              Desenvolvimento de Sistemas e focando em me especializar como
+              Desenvolvedor Front-End. Estou em busca de oportunidades para
+              aplicar meus conhecimentos e contribuir para projetos inovadores.
+            </AtomsParagraph>
+            {!breakpoint.md && (
+              <AtomsPeopleSVG className="mx-auto max-w-[400px] sm:max-w-[450px]" />
+            )}
+          </section>
+          <section>
+            <MoleculesSkills skills={skills} />
+          </section>
+        </div>
+        {breakpoint.md && <AtomsPeopleSVG className="max-w-[450px]" />}
       </main>
     </TemplatesMain>
   );
